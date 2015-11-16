9a4fa49de6c72f33be2a4fdf9a02a7d6f8f46613
Hui Li <mdhuilee@gmail.com>
2015-11-15 20:48:05 -0500

    updates

---
+++ Web technology with R/Shiny web app/deploy.r
@@ -0,0 +1,9 @@
+shinyapps::setAccountInfo(name='huili', token='B7D36F3931099EBAC409727865EC52E0', secret='nDiD4RdJiOgHx3U/T/seBhm0xIDSAHKYzF4CktiM')
+
+
+shinyapps::deployApp('/Users/lei/Desktop/one/Practice/GIT_first/Projects/Web technology with R/Shiny web app'
+                     , appName = "Web_tech")
+
+
+shinyapps::deployApp('/Users/lei/Desktop/one/Practice/GIT_first/Projects/Database with R/Shiny web app'
+                     , appName = "database")
